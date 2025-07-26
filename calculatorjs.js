441e3260c09807e70c938fb061c9843fc02c9b1d
Dharunikapattabiraman <dharunikapattabiraman@gmail.com>
2025-07-26 19:46:51 +0530

    Add files via upload

---
+++ calculatorjs.js
@@ -0,0 +1,34 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  if (display.innerText === "0") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  let current = display.innerText;
+  if (current.length > 1) {
+    display.innerText = current.slice(0, -1);
+  } else {
+    display.innerText = "0";
+  }
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch (e) {
+    display.innerText = "Error";
+  }
+}
+
+document.getElementById("theme-toggle").addEventListener("change", function () {
+  document.body.classList.toggle("dark");
+});
